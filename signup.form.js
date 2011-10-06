8fd9200c1544abc5af3e9321029560adf2aa664f
John Paul Hayes <jpshayes@gmail.com>
2011-10-06 16:06:30 -0500

    first pass at using cookie to populate email field on form after being redirected from block sign up

--- signup.form.js
+++ signup.form.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
 	$('#signup-data-form, #signup-data-block').validate();
+	$('#signup-data-block .form-submit').click(function(){
+		var email = $('#edit-email').val()
+		$.cookie('signup', email, { expires: 7, path: '/' });
+	});
+	var cookie = $.cookie('signup');
+	$('#signup-data-form #edit-email').val(cookie)
+	
 });
